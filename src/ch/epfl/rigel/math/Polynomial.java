26cdb60fb291648793c40705d721551ff2dfe20a
jaernifisher <juliette.aerniggmail.com>
2020-02-19 23:51:13 +0100

    3.7 - Polynomial (pas fini) test du constructeur -> assistants à revoir la methode of début toString

--- src/ch/epfl/rigel/math/Polynomial.java
+++ src/ch/epfl/rigel/math/Polynomial.java
@@ -4,10 +4,24 @@ public final class Polynomial {
 
     private double [] coeff;
 
+    /**
+     * Créer un tableau de taille N contenant les coefficients du polynôme à l'index de leur puissance (:= par odre croissant)
+     *
+     * @param coefficientN coefficient de la puissance N
+     *
+     * @param coefficients tableau des coeffficient N-1 à 0 par odre décroissant
+     */
     private Polynomial(double coefficientN , double... coefficients){
-        for (double c : coefficients) {
+
+        coeff = new double [coefficients.length +1];
+        int i=0;
+        while (i<= coefficients.length) {
+            for (double c : coefficients) {
+                coeff[i] = c;
+                i++;
+            }
         }
-            this.coeff= new double[coefficients.length +1];
+        coeff[coefficients.length + 1] = coefficientN;
 
 
 
@@ -15,7 +29,21 @@ public final class Polynomial {
 
     public static Polynomial of(double coefficientN , double... coefficients){
 
+     if (coefficientN==0) {
+        // vérifier la condition
+         throw new IllegalArgumentException();
+     }
+    // question de style?...
+     else return new Polynomial(coefficientN,coefficients);
     }
 
+    /**
+     * TODO implémenter selon pdf
+     * @return
+     */
+    @Override
+    public String toString() {
 
+        return super.toString();
+    }
 }
