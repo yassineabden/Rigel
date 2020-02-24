1384b94633a07250ef6a190b8d9d9deddc718be2
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-02-24 14:52:50 +0100

    Classe GeographicCoordinatesTest créé!

--- test/ch/epfl/rigel/coordinates/GeographicCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/GeographicCoordinatesTest.java
@@ -1,41 +1,4 @@
 package ch.epfl.rigel.coordinates;
-import ch.epfl.test.TestRandomizer;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class GeographicCoordinatesTest {
-
-    @Test
-    void ofWorksWithValidValues(){
-    GeographicCoordinates gc= GeographicCoordinates.ofDeg(100,40);
-    assertEquals(100,gc.lonDeg());
-    assertEquals(40,gc.latDeg()); }
-
-    @Test
-    void ofFailsOnInvalidValues() {
-    assertThrows(IllegalArgumentException.class,()->{
-        GeographicCoordinates.ofDeg(380,80);
-        });
-    assertThrows(IllegalArgumentException.class,()->{
-        GeographicCoordinates.ofDeg(120,-100);
-        });
-    }
-
-    @Test
-    void isValidLonDegWithValidValues(){
-    GeographicCoordinates gc2= GeographicCoordinates.ofDeg(100,40);
-    assertTrue(GeographicCoordinates.isValidLatDeg(40));
-    assertTrue(GeographicCoordinates.isValidLonDeg(100));
-    }
-
-    /*@Test
-    void isValidLonDegWithInValidValues(){
-        GeographicCoordinates gc3= GeographicCoordinates.ofDeg(200,-100);
-        assertFalse(GeographicCoordinates.isValidLonDeg(200));
-        assertFalse(GeographicCoordinates.isValidLatDeg(-100));
-
-    } */
-
-
-
 }
