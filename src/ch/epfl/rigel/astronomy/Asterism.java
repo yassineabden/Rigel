2cbc38c50a2592043ae1cb3b76b1849b68e4bacc
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-03-13 13:37:31 +0100

    Classe astérisme terminé!

---
+++ src/ch/epfl/rigel/astronomy/Asterism.java
@@ -0,0 +1,63 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.Preconditions;
+
+import java.util.List;
+/**
+ * L'astérisme
+ * @author Yassine Abdennadher (299273)
+ * @author Juliette Aerni (296670)
+ */
+public final class Asterism  {
+
+    private List<Star> stars;
+
+    /**
+     * Constructeur qui initialise la liste d'étoiles
+     * @param stars liste d'étoiles
+     * @throws IllegalArgumentException si la liste d'étoiles est vide.
+     */
+    public Asterism(List<Star> stars){
+        Preconditions.checkArgument(!stars.isEmpty());
+        this.stars=stars; }
+
+    /**
+     * Méthode d'accés permettant d'obtenir la liste d'étoiles
+     * Retourne la liste des étoiles formant l'astérisme.
+     *  @return la liste des étoiles formant l'astérisme.
+     */
+    public List<Star> stars(){
+        return stars;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
