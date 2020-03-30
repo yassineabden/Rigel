3d397a950ba072abd9e919371edd4383d7b977c4
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-03-30 23:19:50 +0200

    HygDatabaseLoader finie!

---
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -0,0 +1,67 @@
+package ch.epfl.rigel.astronomy;
+
+
+
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+
+import java.io.*;
+import static java.nio.charset.StandardCharsets.US_ASCII;
+/**
+ * Chargeur de catalogue HYG
+ *
+ * @author Yassine Abdennadher (299273)
+ * @author Juliette Aerni (296670)
+ */
+public enum HygDatabaseLoader implements StarCatalogue.Loader {
+
+    INSTANCE;
+
+    // Index des colonnes dont les informations doivent être extraites
+    private final static int HIP = 2;
+    private final static int PROPER=7;
+    private final static int MAG= 14;
+    private final static int CI= 17;
+    private final static int RARAD = 24;
+    private final static int DECRAD = 25;
+    private final static int BAYER = 28;
+    private final static int CON = 30;
+
+
+    /**
+     * Charge les étoiles et/ou asterism du flot d'entrée et les ajoute au catalogue en cours de construction du bâtisseur
+     * ou lève une IOException s'il y a une erreur
+     * @param inputStream Flot d'entrée contenant des asterism et/ou des étoiles
+     * @param builder Catalogue en cours de construction
+     *
+     * @throws IOException s'il y'a une erreur d'entrée/sortie.
+     */
+    @Override
+    public  void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
+        try ( BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,US_ASCII))){
+
+            String s=bufferedReader.readLine();
+
+            while (s!=null){
+                String[] parts= s.split(",");
+                int hipparcosId= parts[HIP].isEmpty()? 0 : Integer.parseInt(parts[HIP]);
+                String con = parts[CON];
+                String bayer = parts[BAYER].isEmpty()? "?": parts[BAYER];
+                String name = parts[PROPER].isEmpty()? bayer+ " " + con: parts[PROPER];
+                double rared = Double.parseDouble(parts[RARAD]);
+                double decred = Double.parseDouble(parts[DECRAD]);
+                double magnitude = parts[MAG].isEmpty()? 0: Double.parseDouble(parts[MAG]);
+                double colorIndex = parts[CI].isEmpty()? 0 : Double.parseDouble(parts[CI]);
+                EquatorialCoordinates coordinates = EquatorialCoordinates.of(rared,decred);
+
+                Star star = new Star(hipparcosId,name,coordinates,(float) magnitude,(float) colorIndex);
+                builder.addStar(star);
+
+            }
+        }
+    }
+
+
+
+
+
+}
--- src/ch/epfl/rigel/astronomy/StarCatalogue.java
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -82,34 +82,70 @@ public final class StarCatalogue {
         return asterismsMap.get(asterism);
     }
 
+    /**
+     * Un bâtisseur de catalogue d'étoiles
+     *
+     * @author Yassine Abdennadher (299273)
+     * @author Juliette Aerni (296670)
+     */
+
 
     public final static class Builder {
         //TODO est-ce que les listes attribut doivent être final ou pas?
         private List<Star> stars = new ArrayList<>();
         private List<Asterism> asterisms= new ArrayList<>();
 
+        /**
+         * Constructeur par défaut qui initialise le bâtisseur de manière à ce que le catalogue
+          en construction soit initialment vide.
+         */
         public Builder() {}
 
+
+        /**
+         * Ajoute l'étoile donnée au catalogue en cours de construction
+         * @param star l'étoile
+         * @return le bâtisseur
+         */
         public Builder addStar(Star star) {
            stars.add(star);
            return this; }
 
+        /**
+         * Retourne une vue non modifiable sur les étoiles du catalogue en cours de construction.
+         * @return une vue non modifiable sur les étoiles du catalogue en cours de construction.
+         */
         public List<Star> stars(){
             return Collections.unmodifiableList(stars); }
 
+        /**
+         * Ajoute l'astérisme donné au catalogue en cours de construction.
+         * @param asterism l'astérisme
+         * @return le bâtisseur
+         */
         public Builder addAsterism (Asterism asterism){
             asterisms.add(asterism);
             return this; }
 
+        /**
+         * Retourne une vue non modifiablesur les astérismes du catalogue en cours de construction.
+         * @return une vue non modifiablesur les astérismes du catalogue en cours de construction.
+         */
         public List<Asterism> asterisms (){
             return Collections.unmodifiableList(asterisms); }
 
-            // A faire!!
 
-        public Builder loadFrom(InputStream inputStream){
+            // TODO
+
+        public Builder loadFrom(InputStream inputStream) throws IOException{
             return null;
         }
 
+
+        /**
+         * Retourne le catalogue contenant les étoiles et astérismes ajoutés jusqu'alors au bâtisseur.
+         * @return le catalogue contenant les étoiles et astérismes ajoutés jusqu'alors au bâtisseur.
+         */
         public StarCatalogue build(){
             return new StarCatalogue(stars,asterisms);
         }
@@ -117,7 +153,7 @@ public final class StarCatalogue {
     }
 
     /**
-     * Interface qui charge des données
+     * Interface imbriquée qui charge des données
      *
      * @author Yassine Abdennadher (299273)
      * @author Juliette Aerni (296670)
@@ -126,7 +162,7 @@ public final class StarCatalogue {
 
         /**
          * Charge les étoiles et/ou asterism du flot d'entrée et les ajoute au catalogue en cours de construction du bâtisseur
-         *  lève une IOException s'il y a une erreur
+           lève une IOException s'il y a une erreur
          *
          * @param inputStream Flot d'entrée contenant des asterism et/ou des étoiles
          * @param builder Catalogue en cours de construction
