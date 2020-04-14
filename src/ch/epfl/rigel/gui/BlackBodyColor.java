cfe2606e01e830b0d8de81327f06a7194a5ba9db
julietteaerni <juliette.aerni@gmail.com>
2020-04-14 15:14:55 +0200

    W.7 - BlackBodyColor post milou

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -6,7 +6,6 @@ import ch.epfl.rigel.math.RightOpenInterval;
 import javafx.scene.paint.Color;
 
 import java.io.*;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,11 +19,9 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 public class BlackBodyColor {
 
 
-    private final static Map < Integer, Color> kelvinToColor = kelvinToColorMap();
-    private final static RightOpenInterval KELVIN_TEMPERATURE = RightOpenInterval.of(1,6);
-    private final static RightOpenInterval DEG  = RightOpenInterval.of(10,15);
+    private final static Map < Integer, Color> colorMap = colorMap();
+    private final static RightOpenInterval DEG = RightOpenInterval.of(10,15);
     private final static RightOpenInterval COLOR_RGB = RightOpenInterval.of(80,87);
-    private final static String deg = "10deg";
 
     private final static ClosedInterval TEMPERATURE_RANGE = ClosedInterval.of(1000, 40000);
 
@@ -42,27 +39,36 @@ public class BlackBodyColor {
     public static Color colorForTemperature (float kelvin){
 
         Preconditions.checkInInterval(TEMPERATURE_RANGE,kelvin);
-        return ColorCatalogue.CATALOGUE.kelvinToColor(kelvin); }
+        return colorMap.get(tempToIndex(validKelvinTemprature(kelvin))); }
+
+    private static int validKelvinTemprature (float kelvin){
+        int temp = Math.round(kelvin/100);
+        return temp*100; }
+
+    private static int tempToIndex (float kelvin){
+        return (int) (kelvin - 1000) / 100; }
 
 
-    private static Map <Integer, Color> kelvinToColorMap(){
+    private static Map <Integer, Color> colorMap(){
 
         Map <Integer, Color> kelvinToColorMap = new HashMap<>();
 
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ColorCatalogue.class.getResourceAsStream("/bbr_color.txt"), US_ASCII))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(BlackBodyColor.class.getResourceAsStream("/bbr_color.txt"), US_ASCII))) {
             String line;
+            int i = 0;
             while ((line = bufferedReader.readLine()) != null) {
                 if (! line.startsWith("#")){
                     //ne regarde que les lignes ne commençant pas part "#", ce sont des commentaires
-                    if (line.regionMatches((int)DEG.low(), deg, 0, (int)DEG.size())){
+
+                    if (line.regionMatches((int)DEG.low(), "10deg", 0, (int)DEG.size())){
                         // ne regarde que les lignes contenant "10deg" à la position donée et transforme les kelvin en
                         // float et la temperature en instance de Color
 
-                        int  kelvinTemp = Integer.parseInt(line.substring((int)KELVIN_TEMPERATURE.low(),(int)KELVIN_TEMPERATURE.high()));
+                       // int  kelvinTemp = Integer.parseInt(line.substring((int)KELVIN_TEMPERATURE.low(),(int)KELVIN_TEMPERATURE.high()));
 
                         Color colorFX = Color.web(line.substring((int)COLOR_RGB.low(), (int)COLOR_RGB.high()));
-
-                        kelvinToColorMap.put(kelvinTemp,colorFX); }
+                        kelvinToColorMap.put(i,colorFX);
+                        i++;}
                 }
             }
             return kelvinToColorMap;
@@ -75,8 +81,8 @@ public class BlackBodyColor {
 
 
      /**
-     * Test ENUM
-     */
+
+
     private enum ColorCatalogue{
 
         CATALOGUE;
@@ -120,4 +126,5 @@ public class BlackBodyColor {
             return temp*100; }
 
     }
+      */
 }
