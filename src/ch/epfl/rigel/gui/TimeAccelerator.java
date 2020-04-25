0f42b6667919462e5a114aafe2bfb2d2c2c04f4b
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-04-25 21:55:08 +0200

    TimeAccelerator finie à check!

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -1,8 +1,29 @@
 package ch.epfl.rigel.gui;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
 /**
  * @author Yassine Abdennadher (299273)
  * @author Juliette Aerni (296670)
  */
+@FunctionalInterface
 public interface TimeAccelerator {
+
+
+    abstract ZonedDateTime adjust(ZonedDateTime simulatedTime, long nanoSeconds);
+
+    public static TimeAccelerator continuous(int alpha) {
+        return (simulatedTime, nanoSeconds) -> simulatedTime.plusNanos(alpha*nanoSeconds);
+
+    }
+    public static TimeAccelerator discrete (int lambda, Duration steps) {
+        long r = 5;
+        long variation = (long) Math.floor(lambda*5);
+        return ((simulatedTime, nanoSeconds) -> simulatedTime.plusNanos(steps.toNanos()*(long)Math.floor(lambda*nanoSeconds)));
+    }
+
+
 }
