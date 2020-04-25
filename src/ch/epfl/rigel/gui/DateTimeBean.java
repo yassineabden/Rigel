bf7a722436313099d390aa87605e33cc664c5bfb
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-04-25 23:50:04 +0200

    DateTimeBean rédigé

---
+++ src/ch/epfl/rigel/gui/DateTimeBean.java
@@ -0,0 +1,72 @@
+package ch.epfl.rigel.gui;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public final class DateTimeBean {
+
+    private ObjectProperty<LocalDate> date = new SimpleObjectProperty<>();
+    private ObjectProperty<LocalTime> time = new SimpleObjectProperty<>();
+    private ObjectProperty<ZoneId> zone = new SimpleObjectProperty<>();
+    private ZonedDateTime zonedDateTime = ZonedDateTime.of(getDate(),getTime(),getZone());
+
+    public ObjectProperty<LocalDate> dateProperty(){ return date; }
+
+    public LocalDate getDate(){ return date.get(); }
+
+    public void setDate( LocalDate newLocalDate){ date.setValue(newLocalDate); }
+
+    public ObjectProperty<LocalTime> timeProperty(){ return time; }
+
+    public LocalTime getTime(){ return time.get(); }
+
+    public void setTime ( LocalTime newLocalTime) { time.setValue(newLocalTime); }
+
+    public ObjectProperty<ZoneId> zoneProperty() { return zone; }
+
+    public ZoneId getZone(){ return zone.get(); }
+
+    public void setZone(ZoneId newZoneId){  zone.setValue(newZoneId); }
+
+    public ZonedDateTime getZonedDateTime(){ return zonedDateTime; }
+
+    public void setZonedDateTime(ZonedDateTime zdt){ this.zonedDateTime= zdt; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
