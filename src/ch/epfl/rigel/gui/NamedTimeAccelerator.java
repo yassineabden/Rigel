52ad0389935ecfa87ace6b905d2aba7613d717fb
julietteaerni <juliette.aerni@gmail.com>
2020-04-25 17:14:31 +0200

    W.9 - TimeAccelerator + NamedTimeAccelerator création de classe

---
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -0,0 +1,29 @@
+package ch.epfl.rigel.gui;
+
+/**
+ * Enumération représentant un accélérateur de temps
+ *
+ * @author Yassine Abdennadher (299273)
+ * @author Juliette Aerni (296670)
+ */
+public enum NamedTimeAccelerator {
+
+    TIMES_1("1x"),
+    TIMES_30("30x"),
+    TIMES_300("300x"),
+    TIMES_3000("3000x"),
+    DAY("jour"),
+    SIDERAL_DAY("jour sidéral");
+
+    private final String name;
+
+    NamedTimeAccelerator(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        String.format(name, )
+        return name;}
+
+    public
+}
---
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -0,0 +1,8 @@
+package ch.epfl.rigel.gui;
+
+/**
+ * @author Yassine Abdennadher (299273)
+ * @author Juliette Aerni (296670)
+ */
+public interface TimeAccelerator {
+}
