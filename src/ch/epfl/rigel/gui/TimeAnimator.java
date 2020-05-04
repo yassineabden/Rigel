eda5bd5caa7b73fd0be2a84b087361495719c534
julietteaerni <juliette.aerni@gmail.com>
2020-05-04 16:50:37 +0200

    W.9 - TimeAnimator presque fini, à voir les secondes

--- src/ch/epfl/rigel/gui/TimeAnimator.java
+++ src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -1,6 +1,9 @@
 package ch.epfl.rigel.gui;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.property.*;
+
+import java.time.ZonedDateTime;
 
 /**
  * Classe représentant une animation du temps
@@ -11,27 +14,87 @@ import javafx.animation.AnimationTimer;
 public final class TimeAnimator extends AnimationTimer {
 
     private final DateTimeBean instantBean;
-    private long animationStartedSince;
+    ZonedDateTime simulatedZoneDateTime;
+    private final ObjectProperty <TimeAccelerator> accelerator = new SimpleObjectProperty<>();
+    private final BooleanProperty running = new SimpleBooleanProperty();
+    private long initialTime;
 
+    /**
+     *
+     * @param dateTimeBean
+     */
     //todo private-public?
-    public  TimeAnimator (DateTimeBean dateTimeBean){
+    public TimeAnimator (DateTimeBean dateTimeBean){
         instantBean = dateTimeBean;
-
+        running.set(false);
+        accelerator.set(null);
+        //initialisation du "temps zéro" à -1 par défault
+        initialTime = -1;
     }
 
+    /**
+     *
+     */
     @Override
     public void start(){
+        //todo set running avant ou après start?
+        running.set(true);
         super.start();
-       animationStartedSince = 0;
     }
 
+    /**
+     *
+     * @param realTime
+     */
     @Override
-    public void handle(long l) {
-        animationStartedSince += l;
+    public void handle(long realTime) {
 
+        if (initialTime == -1){
+            initialTime = realTime;
+            simulatedZoneDateTime = instantBean.getZonedDateTime();
+        }
+        else {
+            instantBean.setZonedDateTime( getAccelerator()
+                    .adjust(simulatedZoneDateTime,realTime - initialTime));
+        }
     }
+
+    /**
+     *
+     */
     @Override
     public void stop() {
         super.stop();
+        running.set(false);
+    }
+
+    /**
+     * Retourne
+     * @return
+     */
+    public ReadOnlyBooleanProperty isRunning(){return running;}
+
+    /**
+     *
+     * @return
+     */
+    public TimeAccelerator getAccelerator() {
+        return accelerator.get();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ObjectProperty<TimeAccelerator> acceleratorProperty() {
+        return accelerator;
+    }
+
+    /**
+     *
+     * @param accelerator
+     */
+    public void setAccelerator(TimeAccelerator accelerator) {
+        this.accelerator.set(accelerator);
     }
 }
