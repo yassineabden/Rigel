50823e2ba90976142dfa63beba190e9c695ad138
julietteaerni <juliette.aerni@gmail.com>
2020-05-08 22:51:02 +0200

    W.10 - changé mais plein de fautes et pas fini 2.0

--- src/ch/epfl/rigel/gui/ObserverLocationBean.java
+++ src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -14,35 +14,42 @@ public final class ObserverLocationBean {
 
     private final DoubleProperty lonDeg;
     private final DoubleProperty latDeg;
-    private final ObservableValue<GeographicCoordinates> coordinates;
+    //todo assistant avait dit abservable value ça pouvait être une bonne idée mais je capte pas comment
+    //private final ObservableValue <GeographicCoordinates> coordinates;
+    private final ObjectBinding <GeographicCoordinates> coordinates;
 
 
     public ObserverLocationBean() {
         this.lonDeg = new SimpleDoubleProperty();
         this.latDeg = new SimpleDoubleProperty();
         //todo on peut caster le bind ??
-        coordinates = Bindings.createObjectBinding( ()-> coordinatesProperty().setValue(GeographicCoordinates.ofDeg(getLonDeg(),getLatDeg())) ,lonDeg,latDeg );
+        //coordinates = Bindings.createObjectBinding( ()-> coordinatesProperty().setValue(GeographicCoordinates.ofDeg(getLonDeg(),getLatDeg())) ,lonDeg,latDeg );
+       //todo ça marche comme ça?
+        coordinates = Bindings.createObjectBinding(()-> GeographicCoordinates.ofDeg(getLonDeg(),getLatDeg()), lonDeg,latDeg);
     }
 
     public DoubleProperty lonDegProperty(){ return lonDeg; }
 
     public double getLonDeg(){ return lonDeg.get(); }
 
-    public void setLonDeg (double newLonDeg){ latDeg.setValue(newLonDeg); }
+    public void setLonDeg (double newLonDeg){ latDeg.set(newLonDeg); }
 
     public DoubleProperty latDegProperty(){ return latDeg; }
 
     public double getLatDeg(){ return latDeg.get(); }
 
-    public void setLatDeg (double newLatDeg){ latDeg.setValue(newLatDeg); }
+    public void setLatDeg (double newLatDeg){ latDeg.set(newLatDeg); }
 
     //todo pas tout compris le délire de observable value
     public ObservableValue<GeographicCoordinates> coordinatesProperty() {
         return coordinates;
     }
-
     public GeographicCoordinates getCoordinates() {
-        return coordinates();
+        return coordinates.get();
+    }
+    public void setCoordinates (GeographicCoordinates geographicCoordinates){
+        setLonDeg(geographicCoordinates.lonDeg());
+        setLatDeg(geographicCoordinates.latDeg());
     }
 
 
--- src/ch/epfl/rigel/gui/SkyCanvasManager.java
+++ src/ch/epfl/rigel/gui/SkyCanvasManager.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.astronomy.CelestialObject;
 import ch.epfl.rigel.astronomy.ObservedSky;
 import ch.epfl.rigel.astronomy.StarCatalogue;
 import ch.epfl.rigel.coordinates.CartesianCoordinates;
@@ -9,27 +10,78 @@ import ch.epfl.rigel.coordinates.StereographicProjection;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.transform.Transform;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Point2D;
 
 public final class SkyCanvasManager {
 
+    private final Canvas canvas;
+    private final DateTimeBean dateTimeBean;
+    private final ObserverLocationBean observerLocationBean;
+    private final ViewingParametersBean viewingParametersBean;
+    //todo lien ou propriété?
+    private final DoubleProperty mouseAzDeg;
+    private final DoubleProperty mouseAltDeg;
+    private final ObjectProperty<CelestialObject> objectUnderMouse;
+
+    private final ObjectBinding <StereographicProjection> projection;
+    private final ObjectBinding <Transform> planeToCanvas;
+    private final ObjectBinding <ObservedSky> observedSky;
+    private final ObjectBinding <HorizontalCoordinates> mouseHorizontalPosition;
+
+
+
+
+
+    public SkyCanvasManager(StarCatalogue starCatalogue,  DateTimeBean dateTimeBean, ObserverLocationBean observerLocationBean, ViewingParametersBean viewingParametersBean) {
+
+        canvas = new Canvas();
+        this.dateTimeBean = dateTimeBean;
+        this.observerLocationBean = observerLocationBean;
+        this.viewingParametersBean = viewingParametersBean;
+
+       //todo dans la lambda on défini ce que ça va être la valeur de projection?
+        projection = Bindings.createObjectBinding(()-> ( new StereographicProjection(viewingParametersBean.getCenter())), viewingParametersBean);
+       //todo la planeToCanvas était une Transform avant, on doit la faire comme dans l'étape 8 ou juste avec un facteur de dilatation?
+        // à mettre en méthode
+        double dilatation = canvas.getWidth()/ projection.get().applyToAngle(viewingParametersBean.getFieldOfViewDeg());
+        // todo je comprends pas pourquoi ça joue pas... je donne bien une Transform non?
+           planeToCanvas = Bindings.createObjectBinding( () -> Transform.affine(dilatation, 0, 0, -dilatation, canvas.getWidth()/2, canvas.getHeight()/2), viewingParametersBean,canvas );
+           // planeToCanvas = Bindings.createObjectBinding(() -> canvas.getWidth()/ projection.get().applyToAngle(viewingParametersBean.getFieldOfViewDeg()))
+
+        observedSky = Bindings.createObjectBinding(() -> new ObservedSky(dateTimeBean.getZonedDateTime(), observerLocationBean.getCoordinates(),projection.get(), starCatalogue), observerLocationBean,dateTimeBean,projection);
+        canvas.addMouseListener();
+        mouseHorizontalPosition = Bindings.createObjectBinding( () ->  );
+
+
+
+    }
+
+
+
+
+
+/**
     private final ObserverLocationBean observerLocationBean;
     private final DateTimeBean dateTimeBean;
     private final ViewingParametersBean viewingParametersBean;
     private final ObjectBinding<StereographicProjection> projection;
     //todo observable property
-    private final DoubleBinding planeToCanvas;
+    private final ObservableValue <Transform> planeToCanvas;
+   // private final DoubleBinding planeToCanvas;
 
     private final ObservableValue <ObservedSky> observedSky; // Observablevalue
     private final ObjectProperty<Point2D> mousePosition;
     private final ObjectBinding<HorizontalCoordinates> mouseHorizontalPosition;
     private final Canvas canvas;
-
 
 
     public SkyCanvasManager(StarCatalogue starCatalogue, ObserverLocationBean observerLocationBean, DateTimeBean dateTimeBean, ViewingParametersBean viewingParametersBean) {
@@ -53,4 +105,10 @@ public final class SkyCanvasManager {
 
 
     }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+ */
+
 }
