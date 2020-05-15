e30b3a7fedf4132f8755371d956f386d7069da2a
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-05-15 14:38:04 +0200

    etape 11 quasi finie

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -1,6 +1,9 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.astronomy.HygDatabaseLoader;
+import ch.epfl.rigel.astronomy.StarCatalogue;
 import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import ch.epfl.rigel.coordinates.HorizontalCoordinates;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
@@ -8,11 +11,14 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.event.EventType;
 import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.converter.LocalTimeStringConverter;
 import javafx.util.converter.NumberStringConverter;
@@ -31,164 +37,223 @@ import java.util.function.UnaryOperator;
 
 public final class Main extends Application {
 
+    private InputStream resourceStream(String resourceName) {
+        return getClass().getResourceAsStream(resourceName);
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        try (InputStream hs = resourceStream("/hygdata_v3.csv")) {
 
-        ObserverLocationBean observerLocationBean =
-                new ObserverLocationBean();
-        observerLocationBean.setCoordinates(
-                GeographicCoordinates.ofDeg(6.57, 46.52));
+            StarCatalogue catalogue = new StarCatalogue.Builder()
+                    .loadFrom(hs, HygDatabaseLoader.INSTANCE)
+                    .build();
 
-        ZonedDateTime when = ZonedDateTime.now();
-        DateTimeBean dateTimeBean = new DateTimeBean();
-        dateTimeBean.setZonedDateTime(when);
+            ObserverLocationBean observerLocationBean =
+                    new ObserverLocationBean();
+            observerLocationBean.setCoordinates(
+                    GeographicCoordinates.ofDeg(6.57, 46.52));
 
-        ViewingParametersBean viewingParametersBean = new ViewingParametersBean();
+            ZonedDateTime when = ZonedDateTime.now();
+            DateTimeBean dateTimeBean = new DateTimeBean();
+            dateTimeBean.setZonedDateTime(when);
 
-        TimeAnimator timeAnimator = new TimeAnimator(dateTimeBean);
-        timeAnimator.acceleratorProperty().set(NamedTimeAccelerator.TIMES_300.getAccelerator());
+            ViewingParametersBean viewingParametersBean = new ViewingParametersBean();
+            viewingParametersBean.setCenter(HorizontalCoordinates.ofDeg(180.000000000001, 15));
+            viewingParametersBean.setFieldOfViewDeg(100);
 
-        Pane skyPane = new Pane();
-        Pane informationPane = new Pane();
-        BorderPane mainPane = new BorderPane();
+            TimeAnimator timeAnimator = new TimeAnimator(dateTimeBean);
+            timeAnimator.acceleratorProperty().set(NamedTimeAccelerator.TIMES_300.getAccelerator());
 
 
-        //BorderPane mainPane = new BorderPane(skyPane,controlPane,null,informationPane,null);
 
-        // Fenêtre principale
-        stage.setMinHeight(600);
-        stage.setTitle("Rigel");
-        stage.setMinWidth(800);
+            BorderPane mainPane = new BorderPane();
 
-        // Barre de contrôle
-        //je pense qu'on va devoir les set children à la controlePane
 
-        //Sous-panneau position d'observation
-        HBox observationPositionPane = new HBox();
-        HBox observationTimePane = new HBox();
-        HBox timeLapsePane = new HBox();
+            //BorderPane mainPane = new BorderPane(skyPane,controlPane,null,informationPane,null);
 
-        Separator separator1 = new Separator(Orientation.VERTICAL);
-        Separator separator2 = new Separator(Orientation.VERTICAL);
+            // Fenêtre principale
+            stage.setMinHeight(600);
+            stage.setTitle("Rigel");
+            stage.setMinWidth(800);
 
-        HBox controlBar = new HBox(observationPositionPane,separator1,observationTimePane,separator2,timeLapsePane);
-        controlBar.setStyle("-fx-spacing: 4; -fx-padding: 4;");
+            // Barre de contrôle
+            //je pense qu'on va devoir les set children à la controlePane
 
-        //observationPositionPane
-        observationPositionPane.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+            //Sous-panneau position d'observation
+            HBox observationPositionPane = new HBox();
+            HBox observationTimePane = new HBox();
+            HBox timeLapsePane = new HBox();
 
-        Label lonLabel = new Label("Longitude (°) :");
-        Label latLabel = new Label("Latitude (°) :");
+            Separator separator1 = new Separator(Orientation.VERTICAL);
+            Separator separator2 = new Separator(Orientation.VERTICAL);
 
-        TextField lonTextField = new TextField();
-        TextField latTextField = new TextField();
-        lonTextField.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
-        latTextField.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
+            HBox controlBar = new HBox(observationPositionPane, separator1, observationTimePane, separator2, timeLapsePane);
+            controlBar.setStyle("-fx-spacing: 4; -fx-padding: 4;");
 
-        NumberStringConverter stringToNumberConverter =
-                new NumberStringConverter("#0.00");
+            //observationPositionPane
+            observationPositionPane.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
 
-        //longitude texte field
-        UnaryOperator<TextFormatter.Change> lonFilter = (change -> {
-            try {
-                String newText =
-                        change.getControlNewText();
-                double newLonDeg =
-                        stringToNumberConverter.fromString(newText).doubleValue();
-                return GeographicCoordinates.isValidLonDeg(newLonDeg)
-                        ? change
-                        : null;
-            } catch (Exception e) {
-                return null;
+            Label lonLabel = new Label("Longitude (°) :");
+            Label latLabel = new Label("Latitude (°) :");
+
+            TextField lonTextField = new TextField();
+            TextField latTextField = new TextField();
+            lonTextField.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
+            latTextField.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
+
+            NumberStringConverter stringToNumberConverter =
+                    new NumberStringConverter("#0.00");
+
+            //longitude texte field
+            UnaryOperator<TextFormatter.Change> lonFilter = (change -> {
+                try {
+                    String newText =
+                            change.getControlNewText();
+                    double newLonDeg =
+                            stringToNumberConverter.fromString(newText).doubleValue();
+                    return GeographicCoordinates.isValidLonDeg(newLonDeg)
+                            ? change
+                            : null;
+                } catch (Exception e) {
+                    return null;
+                }
+            });
+
+            TextFormatter<Number> lonTextFormatter =
+                    new TextFormatter<>(stringToNumberConverter, 0, lonFilter);
+
+            lonTextField.setTextFormatter(lonTextFormatter);
+
+            // latitude text field
+            UnaryOperator<TextFormatter.Change> latFilter = (change -> {
+                try {
+                    String newText =
+                            change.getControlNewText();
+                    double newLatDeg =
+                            stringToNumberConverter.fromString(newText).doubleValue();
+                    return GeographicCoordinates.isValidLatDeg(newLatDeg)
+                            ? change
+                            : null;
+                } catch (Exception e) {
+                    return null;
+                }
+            });
+
+            TextFormatter<Number> latTextFormatter =
+                    new TextFormatter<>(stringToNumberConverter, 0, latFilter);
+
+            latTextField.setTextFormatter(latTextFormatter);
+
+            observationPositionPane.getChildren().addAll(lonLabel, lonTextField, latLabel, latTextField);
+
+            //TODO check avec Ju bind value de lonTextFormatter et la position de l'observateur
+            //sans doutes ouais mais il faut def ce qui va se passer?
+
+            lonTextFormatter.valueProperty().bind(observerLocationBean.lonDegProperty());
+            latTextFormatter.valueProperty().bind(observerLocationBean.latDegProperty());
+
+
+            //instant d'observation HBox
+            observationTimePane.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+
+            //Date field
+            Label dateText = new Label("Date :");
+            DatePicker datePicker = new DatePicker();
+            datePicker.setStyle("-fx-pref-width: 120;");
+            datePicker.valueProperty().bind(dateTimeBean.dateProperty());
+
+            //todo peut -être les liés avant de les setChildren à observationTimePane?
+
+            // Hour Field
+            Label hourLabel = new Label("Heure :");
+            TextField hourTextField = new TextField();
+            hourTextField.setStyle("-fx-pref-width: 75; -fx-alignment: baseline-right;");
+
+            DateTimeFormatter hmsFormatter =
+                    DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalTimeStringConverter stringConverter =
+                    new LocalTimeStringConverter(hmsFormatter, hmsFormatter);
+            TextFormatter<LocalTime> timeFormatter =
+                    new TextFormatter<>(stringConverter);
+            timeFormatter.valueProperty().bind(dateTimeBean.timeProperty());
+
+            // todo Time zone pane
+
+            //time lapse pane
+
+            timeLapsePane.setStyle("-fx-spacing: inherit;");
+            ChoiceBox<NamedTimeAccelerator> timeAcceleratorChoiceBox = new ChoiceBox<>();
+            timeAcceleratorChoiceBox.setItems(FXCollections.observableArrayList(NamedTimeAccelerator.values()));
+
+            //todo bidirectional
+            timeAcceleratorChoiceBox.valueProperty().bind(Bindings.select(timeAnimator.acceleratorProperty(), "name"));
+            timeAnimator.acceleratorProperty().bind(Bindings.select(timeAcceleratorChoiceBox.valueProperty(), "accelerator"));
+
+            try (InputStream fontStream = getClass()
+                    .getResourceAsStream("/Font Awesome 5 Free-Solid-900.otf")) {
+
+                Font fontAwesome = Font.loadFont(fontStream, 15);
+                String reset = "\uf0e2";
+                Button resetButton = new Button(reset);
+                resetButton.setFont(fontAwesome);
+
+                String pause = "\uf04b";
+                Button pauseButton = new Button(pause);
+                pauseButton.setFont(fontAwesome);
+
+                String play = "\uf04c";
+                Button playButton = new Button(play);
+                playButton.setFont(fontAwesome);
             }
-        });
 
-        TextFormatter<Number> lonTextFormatter =
-                new TextFormatter<>(stringToNumberConverter, 0, lonFilter);
+            //todo gérer pause/play
 
-        lonTextField.setTextFormatter(lonTextFormatter);
+            // Ciel
+            SkyCanvasManager canvasManager = new SkyCanvasManager(
+                    catalogue,
+                    dateTimeBean,
+                    observerLocationBean,
+                    viewingParametersBean);
 
-        // latitude text field
-        UnaryOperator<TextFormatter.Change> latFilter = (change -> {
-            try {
-                String newText =
-                        change.getControlNewText();
-                double newLatDeg =
-                        stringToNumberConverter.fromString(newText).doubleValue();
-                return GeographicCoordinates.isValidLatDeg(newLatDeg)
-                        ? change
-                        : null;
-            } catch (Exception e) {
-                return null;
-            }
-        });
+            Canvas sky = canvasManager.canvas();
+            Pane skyPane = new Pane(sky);
 
-        TextFormatter<Number> latTextFormatter =
-                new TextFormatter<>(stringToNumberConverter, 0, latFilter);
+            sky.widthProperty().bind(skyPane.widthProperty());
+            sky.heightProperty().bind(skyPane.heightProperty());
 
-        latTextField.setTextFormatter(latTextFormatter);
+            mainPane.setCenter(skyPane);
 
-        observationPositionPane.getChildren().addAll(lonLabel,lonTextField,latLabel,latTextField);
+            // Barre d'information
 
-        //TODO check avec Ju bind value de lonTextFormatter et la position de l'observateur
-        //sans doutes ouais mais il faut def ce qui va se passer?
+            Text fieldOfViewText = new Text();
+            fieldOfViewText.textProperty().bind(Bindings.format("Champ de vue : %.2f °",viewingParametersBean.getFieldOfViewDeg()));
 
-        lonTextFormatter.valueProperty().bind(observerLocationBean.lonDegProperty());
-        latTextFormatter.valueProperty().bind(observerLocationBean.latDegProperty());
+            Text mousePositionText = new Text();
+            mousePositionText.textProperty().bind(Bindings.format("Azimut : %.2f°, hauteur : %2.f°",canvasManager.getMouseAzDeg(),canvasManager.getMouseAltDeg()));
+
+            Text objectClosesToText = new Text();
+            objectClosesToText.textProperty().bind(Bindings.format("%s",canvasManager.getObjectUnderMouse().info()));
+
+            BorderPane informationPane = new BorderPane(objectClosesToText,null,mousePositionText,null,fieldOfViewText);
+
+            informationPane.setStyle("-fx-padding: 4; -fx-background-color: white;");
 
 
-        //instant d'observation HBox
-        observationTimePane.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+            mainPane.setBottom(informationPane);
 
-        //Date field
-        Label dateText = new Label("Date :");
-        DatePicker datePicker = new DatePicker();
-        datePicker.setStyle("-fx-pref-width: 120;");
-        datePicker.valueProperty().bind(dateTimeBean.dateProperty());
+            stage.setScene(new Scene(mainPane));
 
-        //todo peut -être les liés avant de les setChildren à observationTimePane?
+            stage.show();
 
-        // Hour Field
-        Label hourLabel = new Label("Heure :");
-        TextField hourTextField = new TextField();
-        hourTextField.setStyle("-fx-pref-width: 75; -fx-alignment: baseline-right;");
-
-        DateTimeFormatter hmsFormatter =
-                DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTimeStringConverter stringConverter =
-                new LocalTimeStringConverter(hmsFormatter, hmsFormatter);
-        TextFormatter<LocalTime> timeFormatter =
-                new TextFormatter<>(stringConverter);
-        timeFormatter.valueProperty().bind(dateTimeBean.timeProperty());
-
-        // todo Time zone pane
-
-        //time lapse pane
-
-        timeLapsePane.setStyle("-fx-spacing: inherit;");
-        ChoiceBox <NamedTimeAccelerator> timeAcceleratorChoiceBox = new ChoiceBox<>();
-        timeAcceleratorChoiceBox.setItems( FXCollections.observableArrayList(NamedTimeAccelerator.values()));
-
-        //todo wtf?
-        timeAcceleratorChoiceBox.valueProperty().bind(Bindings.select(timeAnimator, "name"));
-        timeAnimator.acceleratorProperty().bind(Bindings.select(timeAcceleratorChoiceBox.valueProperty(), "accelerator"));
-
-        //Bouton reset play pause
-        InputStream fontStream = getClass()
-                .getResourceAsStream("/Font Awesome 5 Free-Solid-900.otf");
-        Font fontAwesome = Font.loadFont(fontStream, 15);
-
-        Button resetButton = new Button("\uf0e2");
-        resetButton.setFont(fontAwesome);
-
+            sky.requestFocus();
+        }
 
     }
-
-
 
 
 }
