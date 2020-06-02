b69d7f4717202cab36f82c36d4efc2e6c500ac20
yassineabdennadher <yasssine.abdennadher@epfl.ch>
2020-06-02 14:01:10 +0200

    Bonus commencé 2.0

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -1,14 +1,12 @@
 package ch.epfl.rigel.gui;
 
 import javafx.application.Application;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -16,8 +14,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.io.FileInputStream;
 
 public class Main extends Application {
 
@@ -42,15 +38,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Image image = new Image("fond.jpg");
-        BackgroundImage backgroundImage = new BackgroundImage(image, null,null,null,null);
-
-
         GridPane mainPane = new GridPane();
-        //mainPane.setPadding(new Insets(10,10,10,10));
-        mainPane.setVgap(5);
         mainPane.setHgap(2);
-        mainPane.setBackground(new Background(backgroundImage));
+        mainPane.setVgap(2);
+        mainPane.setPadding(new Insets(10));
+
+        Image image = new Image("sky.jpg");
+        BackgroundImage backgroundImage = new BackgroundImage(image, null,null,null,null);
+       mainPane.setBackground(new Background(backgroundImage));
 
         stage.setMinHeight(MIN_HEIGHT);
         stage.setTitle(TITLE);
@@ -62,27 +57,27 @@ public class Main extends Application {
         Text textTitle = new Text("Rigel.");
         textTitle.setFont(Font.font(familyTitle,sizeTitle));
         textTitle.setFill(Color.web("#D17008"));
-        mainPane.add(textTitle,125,9);
+        mainPane.add(textTitle,125,15);
 
-       Text textButton = new Text("Observer le ciel");
-       double sizetextButton = 50;
-       String familyButton = "Rockwell";
-       textButton.setFont(Font.font(familyButton,sizetextButton));
-       Button button = new Button(textButton.getText());
+        Text textButton = new Text("Observer le ciel");
+        double sizetextButton = 50;
+        String familyButton = "Rockwell";
+        textButton.setFont(Font.font(familyButton,sizetextButton));
+        Button button = new Button(textButton.getText());
+
 
        button.setStyle("-fx-background-color: \n" +
-               "        linear-gradient(#ffd65b, #e68400),\n" +
-               "        linear-gradient(#ffef84, #f2ba44),\n" +
-               "        linear-gradient(#ffea6a, #efaa22),\n" +
-               "        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),\n" +
-               "        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));\n" +
-               "    -fx-background-radius: 30;\n" +
-               "    -fx-background-insets: 0,1,2,3,0;\n" +
-               "    -fx-text-fill: #654b00;\n" +
-               "    -fx-font-weight: bold;\n" +
-               "    -fx-font-size: 25px;\n" +
-               "    -fx-padding: 10 20 10 20;");
-       mainPane.add(button,125,11);
+               "        #000000,\n" +
+               "        linear-gradient(#7ebcea, #2f4b8f),\n" +
+               "        linear-gradient(#426ab7, #263e75),\n" +
+               "        linear-gradient(#395cab, #223768);\n" +
+               "    -fx-background-insets: 0,1,2,3;\n" +
+               "    -fx-background-radius: 3,2,2,2;\n" +
+               "    -fx-padding: 12 30 12 30;\n" +
+               "    -fx-text-fill: white;\n" +
+               "    -fx-font-size: 25px;");
+
+        mainPane.add(button,125,30);
 
         Text textButton2 = new Text("En savoir plus...");
         double sizetextButton2 = 50;
@@ -91,20 +86,21 @@ public class Main extends Application {
         Button button2 = new Button(textButton2.getText());
 
         button2.setStyle("-fx-background-color: \n" +
-                "        linear-gradient(#ffd65b, #e68400),\n" +
-                "        linear-gradient(#ffef84, #f2ba44),\n" +
-                "        linear-gradient(#ffea6a, #efaa22),\n" +
-                "        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),\n" +
-                "        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));\n" +
-                "    -fx-background-radius: 30;\n" +
-                "    -fx-background-insets: 0,1,2,3,0;\n" +
-                "    -fx-text-fill: #654b00;\n" +
-                "    -fx-font-weight: bold;\n" +
-                "    -fx-font-size: 25px;\n" +
-                "    -fx-padding: 10 20 10 20;");
-        mainPane.add(button2,125,13);
+                "        #000000,\n" +
+                "        linear-gradient(#7ebcea, #2f4b8f),\n" +
+                "        linear-gradient(#426ab7, #263e75),\n" +
+                "        linear-gradient(#395cab, #223768);\n" +
+                "    -fx-background-insets: 0,1,2,3;\n" +
+                "    -fx-background-radius: 3,2,2,2;\n" +
+                "    -fx-padding: 12 30 12 30;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-font-size: 25px;");
+        mainPane.add(button2,125,40);
 
-        EventHandler<MouseEvent> eventHandler = mouseEvent -> {
+
+
+
+        EventHandler<MouseEvent> eventButton1 = mouseEvent -> {
             try {
                 finalSky.start(stage);
             } catch (Exception e) {
@@ -112,7 +108,14 @@ public class Main extends Application {
             }
         };
 
-        button.addEventFilter(MouseEvent.MOUSE_CLICKED,eventHandler);
+        EventHandler<MouseEvent> eventButton2 = mouseEvent -> {
+            showTextButton2(stage);
+        };
+        button.addEventFilter(MouseEvent.MOUSE_CLICKED,eventButton1);
+        button2.addEventFilter(MouseEvent.MOUSE_CLICKED,eventButton2);
+
+
+
 
 
 
@@ -122,4 +125,60 @@ public class Main extends Application {
         stage.setScene(new Scene(mainPane));
         stage.show();
     }
+
+
+    private void showTextButton2 ( Stage stage) {
+
+        BorderPane borderPane = new BorderPane();
+        Image image = new Image("sky.jpg");
+        BackgroundImage backgroundImage = new BackgroundImage(image, null,null,null,null);
+        borderPane.setBackground(new Background(backgroundImage));
+
+        Text text = new Text("Ce travail a été réalisé dans " +
+                "le cadre d’un cours donné à l’EPFL" +
+                " par Yassine Abdennadher et Juliette Aerni," +
+                " deux jeunes programmeurs qui ne sont qu’à" +
+                " leur début dans le monde de la programmation. " +
+                "On espère que vous allez profiter de leur magnifique travail. " +
+                "Hésitez pas non plus à envoyer votre avis aux adresses mail: " +
+                "yassine.abdennadher@epfl.ch ou juliette.aerni@epfl.ch.");
+
+        stage.setHeight(MIN_HEIGHT);
+        stage.setTitle(TITLE);
+        stage.setWidth(MIN_WIDTH);
+        TextField textField = new TextField("Ce travail a été réalisé dans " +
+                "le cadre d’un cours donné à l’EPFL" +
+                " par Yassine Abdennadher et Juliette Aerni," +
+                " deux jeunes programmeurs qui ne sont qu’à" +
+                " leur début dans le monde de la programmation. " +
+                "On espère que vous allez profiter de leur magnifique travail. " +
+                "Hésitez pas non plus à envoyer votre avis aux adresses mail: " +
+                "yassine.abdennadher@epfl.ch ou juliette.aerni@epfl.ch.");
+        double size = 18;
+        String family = "Rockwell";
+        text.setFont(Font.font(family,size));
+
+
+
+        borderPane.setCenter(text);
+
+        stage.setScene(new Scene(borderPane));
+        stage.show();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
